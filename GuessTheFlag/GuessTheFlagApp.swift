72b67667515e3a6b8e7aed51c04f0195ebdaa419
Carlos Eduardo Witte <cewitte@icloud.com>
2024-07-05 08:52:28 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Carlos Eduardo Witte on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
